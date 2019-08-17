fc0c6a8f3490d233e26b221dc630f3f08dc475f5
klupzor <lopezgand@gmail.com>
2019-08-17 00:20:46 +0000

    adding spaces

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,16 +1,16 @@
-#puppet file to config server
+# puppet file to config server
 exec { 'update':
-        command  => 'sudo apt-get update',
-        provider =>shell,
+  command  => 'sudo apt-get update',
+  provider => shell,
 }
 -> package{'nginx':
-  ensure =>present,
+  ensure => present,
 }
-->file_line {'insert_line':
+-> file_line {'insert_line':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   line   => "	location / {
-  add_header X-Served-By ${hostname};",
+  	add_header X-Served-By ${hostname};",
   match  => '^/tlocation / {',
 }
 -> exec { 'restart service nginx':
