acd903b5c3d070b6cf55c869f88027df5924c712
klupzor <lopezgand@gmail.com>
2019-08-17 00:15:42 +0000

    adding style

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,6 +1,6 @@
 #puppet file to config server
 exec { 'update':
-        command => 'sudo apt-get update',
+        command  => 'sudo apt-get update',
         provider =>shell,
 }
 -> package{'nginx':
@@ -8,12 +8,12 @@ exec { 'update':
 }
 ->file_line {'insert_line':
   ensure => present,
-  path => '/etc/nginx/sites-available/default',
-  line => "	location / {
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
   add_header X-Served-By ${hostname};",
-  match => '^/tlocation / {',
+  match  => '^/tlocation / {',
 }
 -> exec { 'restart service nginx':
-  command => 'sudo service nginx restart',
+  command  => 'sudo service nginx restart',
   provider => shell,
 }
