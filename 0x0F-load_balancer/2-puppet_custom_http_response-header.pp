f99b2a6a997b661edb1771cc954f76e4082c74b9
klupzor <lopezgand@gmail.com>
2019-08-17 00:10:02 +0000

    puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,16 @@
+#puppet file to config server
+exec { 'update':
+        command => 'sudo apt-get update',
+        provider =>shell,
+}
+->file_line {'insert_line':
+  ensure => present,
+  path => '/etc/nginx/sites-available/default',
+  line => " location / {
+  add_header X-Served-By ${hostname};"
+  match => '^/tlocation / {',
+}
+-> exec { 'restart service nginx'
+  command => 'sudo service nginx restart',
+  provider => shell,
+}
