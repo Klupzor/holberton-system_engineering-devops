18505c7ebaef6bb4bc12ef323e4e12368ca783cc
klupzor <lopezgand@gmail.com>
2019-08-17 00:12:24 +0000

    fixing

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,6 +3,9 @@ exec { 'update':
         command => 'sudo apt-get update',
         provider =>shell,
 }
+-> package{'nginx':
+  ensure =>present,
+}
 ->file_line {'insert_line':
   ensure => present,
   path => '/etc/nginx/sites-available/default',
