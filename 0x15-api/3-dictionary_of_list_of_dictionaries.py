aea68786b7fce5acafa6d6bd7d640d60005edab7
klupzor <lopezgand@gmail.com>
2019-09-16 23:20:47 +0000

    task 3 finished

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Export to JSON"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    request = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(request)
+    try:
+        users = response.json()
+    except BaseException:
+        print("Not a valid JSON")
+    request = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(request)
+    try:
+        tasks = response.json()
+    except BaseException:
+        print("Not a valid JSON")
+    values = []
+    for user in users:
+        user_id = user.get("id")
+        for task in tasks:
+            if task.get("userId") == user_id:
+                values.append({"task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": user.get("username")})
+        to_dict = {user_id: values}
+
+    fileName = "todo_all_employees.json"
+    with open(fileName, 'w') as employee_file:
+        json.dump(to_dict, employee_file)
