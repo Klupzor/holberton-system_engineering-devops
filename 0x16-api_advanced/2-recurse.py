ef7d57daf997a65275a1dae2a9a290568d440012
klupzor <lopezgand@gmail.com>
2019-09-19 21:16:47 +0000

    recursive

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Gather data from an reddit API"""
+import requests
+import sys
+
+
+def recurse(subreddit, after=None, hot_list=[]):
+    """Gather data from an reddit API"""
+    base_url = "https://www.reddit.com"
+    url = base_url + "/r/{}/hot.json".format(subreddit)
+    headers = {'user-agent': 'Andres'}
+    params = {'limit': 100, 'after': after}
+    response = requests.get(
+        url,
+        params=params,
+        headers=headers,
+        allow_redirects=False)
+
+    if response.status_code != 200:
+        return (None)
+    jsn = response.json()
+    posts = jsn.get("data").get("children")
+    for post in posts:
+        hot_list.append(post.get("data").get("title"))
+    after = jsn.get("data").get("after")
+    if after is None:
+        return (None)
+    recurse(subreddit, after)
+    return (hot_list)
