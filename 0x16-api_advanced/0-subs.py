980d7a6fcea6230b89ae6d5985ee1b3c7223bbf6
klupzor <lopezgand@gmail.com>
2019-09-19 17:59:15 +0000

    request subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Gather data from an reddit API"""
+import requests
+import sys
+
+def number_of_subscribers(subreddit):
+    base_url = "https://www.reddit.com"
+    url = base_url + "/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'Andres'}
+    response = requests.get(url, headers=headers)
+    try:
+        jsn = response.json()
+    except BaseException:
+        print("Not a valid JSON")
+    else:
+        if jsn:
+            return (jsn.get("data").get("subscribers"))
+        else:
+            return (0)
