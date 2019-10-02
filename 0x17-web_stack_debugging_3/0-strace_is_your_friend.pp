b5082a39f7c2476d63ae25c7f016b695115e01b0
klupzor <lopezgand@gmail.com>
2019-10-02 21:53:23 +0000

    only search extension file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,5 @@
 exec { 'fix-file':
   path     => '/usr/bin:/usr/sbin:/bin',
   provider => shell,
-  command  => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g'\
-/var/www/html/wp-settings.php",
+  command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
