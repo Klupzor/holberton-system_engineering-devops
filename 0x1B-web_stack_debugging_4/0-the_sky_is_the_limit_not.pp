c2c170faa99c9217d1141c626a7e4042fc2f3a43
klupzor <lopezgand@gmail.com>
2019-10-09 21:42:55 +0000

    adding restart nginx service

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,6 +2,5 @@
 exec { 'fix-file':
   path     => '/usr/bin:/usr/sbin:/bin',
   provider => shell,
-  command  => "sed -i 's/-n 15/-n 2019/g' /etc/default/nginx",
-  command  => "sudo service nginx restart",
+  command  => "sed -i 's/-n 15/-n 2019/g' /etc/default/nginx && sudo service nginx restart",
 }
