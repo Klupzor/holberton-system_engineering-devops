46a8b6e556dbbacb099f64c0d1f01580f1657e6d
klupzor <lopezgand@gmail.com>
2019-10-09 21:38:53 +0000

    Fixing line

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# Fix request
+exec { 'fix-file':
+  path     => '/usr/bin:/usr/sbin:/bin',
+  provider => shell,
+  command  => "sed -i 's/-n 15/-n 2019/g' /etc/default/nginx",
+  command  => "sudo service nginx restart",
+}
